29c7b26ef207216a0d37ca3dbc6edfd7a3b8b8c4
orangleLi <18291003389@163.com>
2020-12-15 17:32:01 +0800

    案例demo01

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# typescript
-typescript学习
+安装typescript
+npm install -g typescript
+
+项目中可以新建ts文件
+编译ts文件生成js  tsc 文件名.ts
+
+vscode中自动编译
+tsc --init 生成tsconfig.json文件
+"outDir": "./" 打开注释，配置编译生成的js文件路径
+"outDir": "./js"
+任务  ->  运行任务  ->  typescript  ->  tsc:监视 tsconfig.json
+
+
---
+++ demo01/js/test.js
@@ -0,0 +1,34 @@
+"use strict";
+// 布尔型
+var boo = true;
+var bo = !!5;
+console.log(boo, bo);
+// 字符串
+var str = 'hello';
+console.log(str);
+// 数组
+var arr = [1, 2, 3];
+var arr1 = ['a', 'b', 'c'];
+console.log(arr, arr1);
+// null undefined
+var _null = null;
+var _undefined = undefined;
+// ts中的或
+var some_var = 12;
+console.log(some_var);
+some_var = true;
+console.log(some_var);
+some_var = 'abc';
+console.log(some_var);
+// any 不进行类型校验
+var _any = 112;
+_any = true;
+// void 函数没有返回值
+function func(args) {
+}
+func(1);
+function func1(args) {
+    // 逻辑代码
+    return true;
+}
+func1('');
