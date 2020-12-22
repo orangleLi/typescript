dac4ef038a976956abcd665b4faed974908ea706
orangleLi <18291003389@163.com>
2020-12-22 09:40:15 +0800

    类

--- demo01/js/基础类型.js
+++ demo01/js/基础类型.js
@@ -38,3 +38,23 @@ var func2 = function (args) {
     return 'hhh';
 };
 func2([1]);
+function getName(n) {
+    if (typeof n === 'string') {
+        return n;
+    }
+    else {
+        return n();
+    }
+}
+function handle(a, b) {
+    console.log(a, b);
+}
+handle('hhh', 'age'); // 第二个参数只能传age或sex或length
+// 元组
+// 数组合并了相同类型的对象，而元组合并了不同类型的对象
+var info_tuples = ['hhh', 1];
+// 注意：当直接对元组类型的变量进行初始化或者赋值的时候,需要提供所有元组类型中指定的项.
+info_tuples.push('abc');
+console.log(info_tuples); // ["hhh", 1, true, "abc"]
+// 当添加越界的元素时，它的类型会被限制为元组中每个类型的联合类型。
+// info_tuples.push(true) // 报错 Argument of type 'boolean' is not assignable to parameter of type 'string | number'.
--- demo01/基础类型.ts
+++ demo01/基础类型.ts
@@ -47,3 +47,33 @@ let func2 = (args: number[]): string => {
   return 'hhh'
 }
 func2([1])
+
+// 类型别名
+// 可以理解为给一个类型起一个新的名字
+type Name = string; // 声明一个变量，返回类型是string
+type NameResolver = () => string; // 声明一个函数，返回类型是string
+type NameOrNameResolver = Name | NameResolver;
+function getName(n: NameOrNameResolver): string {
+  if (typeof n === 'string') {
+    return n;
+  } else {
+    return n();
+  }
+}
+
+
+// 字符串字面量类型
+type Info = 'age' | 'sex' | 'length'
+function handle(a: string, b: Info): void {
+  console.log(a, b);
+}
+handle('hhh', 'age') // 第二个参数只能传age或sex或length
+
+// 元组
+// 数组合并了相同类型的对象，而元组合并了不同类型的对象
+let info_tuples: [string, number] = ['hhh', 1];
+// 注意：当直接对元组类型的变量进行初始化或者赋值的时候,需要提供所有元组类型中指定的项.
+info_tuples.push('abc')
+console.log(info_tuples) // ["hhh", 1, true, "abc"]
+// 当添加越界的元素时，它的类型会被限制为元组中每个类型的联合类型。
+// info_tuples.push(true) // 报错 Argument of type 'boolean' is not assignable to parameter of type 'string | number'.
